16eafe7edd9b4e9cf6aaf2f0508eb6ebf65e2178
Suryansh Soni <146843858+sonisuryansh@users.noreply.github.com>
2025-10-02 11:22:43 +0530

    Add files via upload

---
+++ tweet.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract TweeterContract{
+    struct Tweet{
+    uint id;
+    address author;
+    string content;
+    uint createdAt;
+    }
+    struct Message{
+        uint id;
+        string content;
+        address from;
+        address to;
+        uint createdAt;
+    }
+
+    mapping (uint => Tweet) public tweets;
+    mapping (address => uint[]) public tweetOf;
+    mapping (address => Message[]) public conversations;
+    mapping (address => mapping(address=>bool)) public operators;
+    mapping (address => address[]) public following;
+
+    uint nextId;
+    uint nextMessageId;
+
+    function _tweet(address _from , string memory _content) internal {
+        tweets[nextId] = Tweet(nextId, _from, _content, block.timestamp);
+        nextId= nextId+1;
+    }
+
+    function _sendMessage(address  _from, address _to, string memory _content) internal {
+        conversations[_from].push(Message(nextMessageId,_content,_from ,_to,block.timestamp));
+        nextMessageId++;
+    }
+
+    function tweet(string memory _content) public{ //Owner 
+        _tweet(msg.sender,_content);
+
+    }
+
+    function tweet (address _from, string memory _content) public { // Owner => access control
+        _tweet(_from,_content);
+    }
+
+    function sendMessage(string memory _content, address _to) public {
+        _sendMessage(msg.sender, _to, _content);
+    }
+
+    function sendMessage(address _from ,address _to, string memory _content) public{
+        _sendMessage(_from, _to, _content);
+    }
+    
+    function follow(address _followed) public {
+        following [msg.sender].push (_followed);
+        
+    }
+
+    function allow (address _operator) public {
+        operators[msg.sender][_operator] = true;
+    }
+
+      function disAllow (address _operator) public {
+        operators[msg.sender][_operator] = false;
+    }
+
+
+    function getLatestTweets(uint count) public view returns (Tweet[] memory){
+        require (count>0 && count <= nextId , "Count is Not Proper");
+
+        Tweet[] memory _tweets = new Tweet[](count);
+
+        uint j;
+
+        for(uint i =nextId-count; i<nextId ;i++ ){
+
+            Tweet storage _structure = tweets[i];
+
+            _tweets[j] =Tweet(
+                _structure.id, 
+                _structure.author, 
+                _structure.content , 
+                _structure.createdAt
+                );
+            j= j+1;
+        }
+        return _tweets;
+    }
+}
